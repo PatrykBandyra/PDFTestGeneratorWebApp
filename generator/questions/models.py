9990959a3678a39b9710db615f47f82340cd350c
Patryk Bandyra <patryk.bandyra@gmail.com>
2021-11-02 20:47:45 +0100

    dashboard upgrade

--- generator/questions/models.py
+++ generator/questions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Subject(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField()
+    ownership = models.ManyToManyField(User, )
